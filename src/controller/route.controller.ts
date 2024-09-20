a95144db1d8197013d4f2ca59c4b5f68c16f6d8a
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2024-09-20 10:29:38 +0900

    ランディングページの作成

--- src/controller/route.controller.ts
+++ src/controller/route.controller.ts
@@ -1,15 +1,15 @@
 import 'reflect-metadata'
 import { Controller, Get, Render } from 'routing-controllers'
-import { AppPages, HomeRenderData } from '../type/route'
+import { AppPages, HomeRenderData, RenderData } from '../type/route'
+
 
 @Controller()
 export class RouteController {
   @Get('/')
-  @Render(AppPages.home)
-  home(): HomeRenderData {
+  @Render(AppPages.landing)
+  landing(): RenderData {
     return {
-      title: 'Home',
-      body: 'Welcome to the home page',
+      title: 'アプリ名',
     }
   }
 }
