229d3a8a438b9f1c0e18034eb540c0c7197a1160
kikukawa <2201009@s.asojuku.ac.jp>
2024-09-25 10:09:33 +0900

    レビュー内容の修正後

---
+++ src/module/user/user.service.ts
@@ -0,0 +1,24 @@
+import { getRepository } from 'typeorm'
+import { hash } from '../../lib/hash' // hash関数をインポート
+import { User } from '../entities/User'
+
+export class UserService {
+  async signup(username: string, email: string, password: string) {
+    const userRepository = getRepository(User)
+
+    const existingUser = await userRepository.findOne({ where: { email } })
+    if (existingUser) {
+      throw new Error('このメールアドレスは既に登録されています。')
+    }
+    const hashedPassword = await hash(password)
+
+    const user = new User()
+    user.username = username
+    user.email = email
+    user.password = hashedPassword
+
+    await userRepository.save(user)
+
+    return user
+  }
+}
