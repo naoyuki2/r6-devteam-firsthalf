c2aad35d623719ebaac9a8cc52a5625562a3fe33
kikukawa <2201009@s.asojuku.ac.jp>
2024-09-26 00:39:25 +0900

    仮データだったのをentityから受け取るよう修正

--- src/module/request/request.service.ts
+++ src/module/request/request.service.ts
@@ -1,38 +1,18 @@
-export class RequestService {
-  private orders = [
-    {
-      id: 101,
-      title: '～に行ってきて',
-      place: '東京',
-      item: '～のライブの限定品',
-      limit: '2024/10/10',
-      description:
-        '東京の10月5日に開かれるライブで販売される限定品を買ってきてください',
-      createdAt: '2024/09/17',
-      user_id: 0,
-    },
-    {
-      id: 102,
-      title: '～に行ってきて',
-      place: '東京',
-      item: '～のライブの限定品',
-      limit: '2024/10/10',
-      description:
-        '東京の10月5日に開かれるライブで販売される限定品を買ってきてください',
-      createdAt: '2024/09/17',
-      user_id: 0,
-    },
-  ]
+import { AppDataSource } from '../../app-data-source'
+import { Request } from './request.entity'
 
+const requestRepository = AppDataSource.getRepository(Request)
+
+export class RequestService {
   async getAll() {
-    return this.orders
+    return requestRepository.find()
   }
 
   async getOneById(id: number) {
-    const order = this.orders.find((order) => order.id === id)
-    if (!order) {
+    const request = requestRepository.findOne({ where: { id } })
+    if (!request) {
       throw new Error(`Order with id ${id} not found`)
     }
-    return order
+    return request
   }
 }
