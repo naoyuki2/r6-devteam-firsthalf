7bd7c88b473f918f0e938ccfaf515efef4ed8551
naoyuki2 <kamaboko912@gmail.com>
2024-09-26 09:29:52 +0900

    諸々調整

--- src/module/request/request.service.ts
+++ src/module/request/request.service.ts
@@ -4,12 +4,12 @@ import { Request } from './request.entity'
 const requestRepository = AppDataSource.getRepository(Request)
 
 export class RequestService {
-  async getAll() {
-    return requestRepository.find()
+  async getAll(): Promise<Request[]> {
+    return await requestRepository.find()
   }
 
-  async getOneById(id: number) {
-    const request = requestRepository.findOne({ where: { id } })
+  async getOneById(id: number): Promise<Request> {
+    const request = await requestRepository.findOne({ where: { id } })
     if (!request) {
       throw new Error(`Order with id ${id} not found`)
     }
