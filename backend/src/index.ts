c49ed67bce8c692b18fba66d912abf6b6d95cb2b
naoyuki2 <kamaboko912@gmail.com>
2024-10-08 14:19:19 +0900

    APIの構成変更

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,13 @@
 import express from 'express'
 import { Action, useExpressServer } from 'routing-controllers'
-import path from 'path'
 import { AppDataSource } from './app-data-source'
 import { RequestController } from './module/request/request.controller'
-import { PageController } from './module/page/page.controller'
 import { UserController } from './module/user/user.controller'
 import { AuthController } from './module/auth/auth.controller'
 import { ErrorHandler } from './middleware/errorHandler'
 import { setCurrentUser } from './middleware/setCurrentUser'
 
-const PORT = 3000
+const PORT = 3030
 
 AppDataSource.initialize()
   .then(() => {
@@ -25,31 +23,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(setCurrentUser)
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'view'))
-
-app.use(
-  '/bootstrap',
-  express.static(path.join(__dirname, '../node_modules/bootstrap/dist')),
-)
-app.use(
-  '/icons',
-  express.static(path.join(__dirname, '../node_modules/bootstrap-icons/font')),
-)
-app.use('/css', express.static(path.join(__dirname, '../public/css')))
-
 useExpressServer(app, {
-  controllers: [
-    PageController,
-    RequestController,
-    UserController,
-    AuthController,
-  ],
-  // middlewares: [ErrorHandler],
+  controllers: [RequestController, UserController, AuthController],
+  middlewares: [ErrorHandler],
   defaultErrorHandler: false,
-  authorizationChecker: (action: Action,roles:string[]) =>{
-    return action.request.currentUser;
-  }
+  authorizationChecker: (action: Action, roles: string[]) => {
+    return action.request.currentUser
+  },
 })
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`))
