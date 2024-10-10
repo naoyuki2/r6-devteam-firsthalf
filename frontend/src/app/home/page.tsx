1b81a36c06ed1718a961a461d08844bcdd5852e2
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2024-10-10 12:40:51 +0900

    デザインの修正

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -14,25 +14,24 @@ export default async function Home() {
       >
         <div className="border-bottom border-opacity-25">
           <div className="d-flex">
-            <PersonCircle size={36} className="me-3" />
-            <span className="fw-bold my-2">平山海翔</span>
-            <span className="ms-auto my-2">2024/10/09 10:38</span>
+            <PersonCircle size={36} className="me-3 mt-2" />
+            <span className="fw-bold my-2 mt-2">平山海翔</span>
+            <span className="ms-auto my-2 mt-2">2024/10/09 10:38</span>
           </div>
-          <p className="fw-bold mb-2">
+          <p className="fw-bold text-truncate mb-2">
             東京ドームのフェス限定のグッズが欲しい！
           </p>
-          <p className="text-truncate mb-2"></p>
 
           <div className="d-flex justify-content-evenly">
             <div className="d-flex align-items-center">
-              <div className="d-flex-flex-column">
+              <div>
                 <HouseDoor size={24} className="ms-2" />
                 <p className="mb-2">HOME</p>
               </div>
               <span className="ms-3">福岡市</span>
             </div>
             <div className="d-flex align-items-center">
-              <div className="d-flex-flex-column">
+              <div>
                 <Shop size={24} className="ms-2" />
                 <p className="mb-2">SHOP</p>
               </div>
