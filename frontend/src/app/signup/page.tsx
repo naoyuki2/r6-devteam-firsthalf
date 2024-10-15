8ea709ae2fb43359e7ce699168a561fa3317bff2
kikukawa <2201009@s.asojuku.ac.jp>
2024-10-15 12:14:50 +0900

    サインアップ機能作成

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,38 +1,11 @@
-'use client'
+import { SignUpClient } from '@/features/signup'
+import { Container } from 'react-bootstrap'
 
-import Link from 'next/link'
-import { Button, Container, Form } from 'react-bootstrap'
-
-export default async function SignUp() {
+export default function Login() {
   return (
     <Container className="vh-100 d-flex justify-content-center align-items-center flex-column">
       <p className="fw-bold fs-2 text-center">新規登録</p>
-      <Form>
-        <Form.Group controlId="email" className="mb-4">
-          <Form.Label className="border-start border-info border-5 ps-2 fw-bold">
-            ユーザーネーム
-          </Form.Label>
-          <Form.Control type="name" placeholder="name"></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="password" className="mb-4">
-          <Form.Label className="border-start border-info border-5 ps-2 fw-bold">
-            メールアドレス
-          </Form.Label>
-          <Form.Control type="email" placeholder="email"></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="password" className="mb-4">
-          <Form.Label className="border-start border-info border-5 ps-2 fw-bold">
-            パスワード
-          </Form.Label>
-          <Form.Control type="password" placeholder="password"></Form.Control>
-        </Form.Group>
-        <Button variant="info" className="w-100">
-          新規登録
-        </Button>
-        <Link href="#" className="link-dark d-block">
-          アカウントをお持ちの方はこちら
-        </Link>
-      </Form>
+      <SignUpClient />
     </Container>
   )
 }
---
+++ frontend/src/features/signup/index.tsx
@@ -0,0 +1,115 @@
+'use client'
+
+import { AppAlert } from '@/component/AppAlert'
+import { AppButton } from '@/component/AppButton'
+import { AppLinkText } from '@/component/AppLinkText'
+import { AppTextInput } from '@/component/AppTextInput'
+import { apiClient } from '@/lib/axios'
+import { setItem } from '@/utils/localStorage'
+import { useRouter } from 'next/navigation'
+import { ChangeEvent, useState } from 'react'
+import { Form } from 'react-bootstrap'
+
+type Form = {
+  name: {
+    value: string
+  }
+  email: {
+    value: string
+  }
+  password: {
+    value: string
+  }
+}
+
+export const SignUpClient = () => {
+  const INITIAL_FORM: Form = {
+    name: { value: '' },
+    email: { value: '' },
+    password: { value: '' },
+  }
+
+  const router = useRouter()
+  const [hasError, setHasError] = useState<boolean>(false)
+  const [form, setForm] = useState<Form>(INITIAL_FORM)
+  const { name, email, password } = form
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setForm((prev) => ({
+      ...prev,
+      [name]: { value },
+    }))
+  }
+
+  const handleLogin = async () => {
+    // TODO : バリデーション, ローディング
+    const args: any = {
+      name: name.value,
+      email: email.value,
+      password: password.value,
+    }
+    try {
+      const res = await apiClient.post('/user', args)
+      if (res == null) return
+      setItem('token', res.data.token)
+      router.push('/home')
+    } catch (e) {
+      console.log(e)
+      setHasError(true)
+    }
+  }
+
+  return (
+    <>
+      <AppAlert
+        message="新規登録に失敗しました"
+        variant="danger"
+        show={hasError}
+      />
+      <Form>
+        <Form.Group controlId="email" className="mb-4">
+          <AppTextInput
+            label="ユーザーネーム"
+            type="text"
+            name="name"
+            placeholder="sample"
+            autoComplete="username"
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="email" className="mb-4">
+          <AppTextInput
+            label="メールアドレス"
+            type="email"
+            name="email"
+            placeholder="hakobun@gmail.com"
+            autoComplete="email"
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="password" className="mb-4">
+          <AppTextInput
+            label="パスワード"
+            type="password"
+            name="password"
+            placeholder="********"
+            autoComplete="password"
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <AppButton
+          text="新規登録"
+          onClick={handleLogin}
+          className="w-100"
+          variant="info"
+        />
+        <AppLinkText
+          text="アカウントをお持ちの方はこちら"
+          href="/login"
+          className="mt-3"
+        />
+      </Form>
+    </>
+  )
+}
