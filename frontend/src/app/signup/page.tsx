0537134d3dad66ab48c6845bfe687efb0bf4d61a
kikukawa <2201009@s.asojuku.ac.jp>
2024-10-15 12:17:35 +0900

    Signup

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 import { SignUpClient } from '@/features/signup'
 import { Container } from 'react-bootstrap'
 
-export default function Login() {
+export default function SignUp() {
   return (
     <Container className="vh-100 d-flex justify-content-center align-items-center flex-column">
       <p className="fw-bold fs-2 text-center">新規登録</p>
