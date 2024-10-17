3bfd4978435e06026124b73c502f7657c019333e
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2024-10-17 12:13:18 +0900

    依頼一覧画面の作成

--- frontend/src/app/(nav)/home/page.tsx
+++ frontend/src/app/(nav)/home/page.tsx
@@ -35,33 +35,37 @@ export default async function Home() {
 
   return (
     <Container>
-      <AppLink href="#">
-        <div className="border-bottom border-opacity-25">
-          <div className="d-flex">
-            <PersonCircle size={36} className="me-3 mt-2" />
-            <span className="fw-bold my-2 mt-2">{requests[0].id}</span>
-            <span className="ms-auto my-2 mt-2"></span>
-          </div>
-          <p className="fw-bold text-truncate mb-2">title</p>
+      <>
+        {requests.map((request: Request) => (
+          <AppLink href="#">
+            <div className="border-bottom border-opacity-25" key={request.id}>
+              <div className="d-flex">
+                <PersonCircle size={36} className="me-3 mt-2" />
+                <span className="fw-bold my-2 mt-2">{request.user.name}</span>
+                <span className="ms-auto my-2 mt-2"></span>
+              </div>
+              <p className="fw-bold text-truncate mb-2">{request.title}</p>
 
-          <div className="d-flex justify-content-evenly">
-            <div className="d-flex align-items-center">
-              <div>
-                <HouseDoor size={24} className="ms-2" />
-                <p className="mb-2">HOME</p>
+              <div className="d-flex justify-content-evenly">
+                <div className="d-flex align-items-center">
+                  <div>
+                    <HouseDoor size={24} className="ms-2" />
+                    <p className="mb-2">HOME</p>
+                  </div>
+                  <span className="ms-3">{request.delivery_prefecture}</span>
+                </div>
+                <div className="d-flex align-items-center">
+                  <div>
+                    <Shop size={24} className="ms-2" />
+                    <p className="mb-2">SHOP</p>
+                  </div>
+                  <span className="ms-3">{request.location_prefecture}</span>
+                </div>
               </div>
-              <span className="ms-3">delivery_prefecture</span>
             </div>
-            <div className="d-flex align-items-center">
-              <div>
-                <Shop size={24} className="ms-2" />
-                <p className="mb-2">SHOP</p>
-              </div>
-              <span className="ms-3">location_prefecture</span>
-            </div>
-          </div>
-        </div>
-      </AppLink>
+          </AppLink>
+        ))}
+      </>
     </Container>
   )
 }
