d91efdd716462ae8df871380ab31b41fd5451cb2
kikukawa <2201009@s.asojuku.ac.jp>
2024-10-17 11:12:37 +0900

    レビュー対応

--- frontend/src/features/request/index.tsx
+++ frontend/src/features/request/index.tsx
@@ -6,83 +6,36 @@ import { useState } from 'react'
 import { Form, Row, Col } from 'react-bootstrap'
 import { apiClient } from '@/lib/axios'
 import { useRouter } from 'next/navigation'
+import { PREFECTURES } from './constants'
+import { AppTextArea } from '@/component/AppTextArea'
+
+type Item = {
+  name: string
+  quantity: number
+  price: string
+}
+
+type Form = {
+  title: string
+  locationPrefecture: string
+  locationDetails: string
+  deliveryPrefecture: string
+  deliveryDetails: string
+  details: string
+  items: Item[]
+}
+
+const INITIAL_FORM: Form = {
+  title: '',
+  locationPrefecture: '',
+  locationDetails: '',
+  deliveryPrefecture: '',
+  deliveryDetails: '',
+  details: '',
+  items: [{ name: '', quantity: 1, price: '' }],
+}
 
 export default function RequestClient() {
-  const prefectures = [
-    '北海道',
-    '青森県',
-    '岩手県',
-    '宮城県',
-    '秋田県',
-    '山形県',
-    '福島県',
-    '茨城県',
-    '栃木県',
-    '群馬県',
-    '埼玉県',
-    '千葉県',
-    '東京都',
-    '神奈川県',
-    '新潟県',
-    '富山県',
-    '石川県',
-    '福井県',
-    '山梨県',
-    '長野県',
-    '岐阜県',
-    '静岡県',
-    '愛知県',
-    '三重県',
-    '滋賀県',
-    '京都府',
-    '大阪府',
-    '兵庫県',
-    '奈良県',
-    '和歌山県',
-    '鳥取県',
-    '島根県',
-    '岡山県',
-    '広島県',
-    '山口県',
-    '徳島県',
-    '香川県',
-    '愛媛県',
-    '高知県',
-    '福岡県',
-    '佐賀県',
-    '長崎県',
-    '熊本県',
-    '大分県',
-    '宮崎県',
-    '鹿児島県',
-    '沖縄県',
-  ]
-
-  type Item = {
-    name: string
-    quantity: number
-    price: string
-  }
-
-  type Form = {
-    title: string
-    locationPrefecture: string
-    locationDetails: string
-    deliveryPrefecture: string
-    deliveryDetails: string
-    details: string
-    items: Item[]
-  }
-
-  const INITIAL_FORM: Form = {
-    title: '',
-    locationPrefecture: '',
-    locationDetails: '',
-    deliveryPrefecture: '',
-    deliveryDetails: '',
-    details: '',
-    items: [{ name: '', quantity: 1, price: '' }],
-  }
   const router = useRouter()
   const [form, setForm] = useState<Form>(INITIAL_FORM)
   const [hasError, setHasError] = useState<boolean>(false)
@@ -169,7 +122,7 @@ export default function RequestClient() {
             onChange={handleInputChange}
           >
             <option value="">選択してください</option>
-            {prefectures.map((prefecture) => (
+            {PREFECTURES.map((prefecture) => (
               <option key={prefecture} value={prefecture}>
                 {prefecture}
               </option>
@@ -198,7 +151,7 @@ export default function RequestClient() {
             onChange={handleInputChange}
           >
             <option value="">選択してください</option>
-            {prefectures.map((prefecture) => (
+            {PREFECTURES.map((prefecture) => (
               <option key={prefecture} value={prefecture}>
                 {prefecture}
               </option>
@@ -217,7 +170,6 @@ export default function RequestClient() {
           />
         </Form.Group>
 
-        {/* アイテムリストの動的追加 */}
         {form.items.map((item, index) => (
           <div key={index}>
             <Form.Group className="mb-3">
@@ -270,6 +222,7 @@ export default function RequestClient() {
                 />
               </Col>
             </Row>
+            <hr />
           </div>
         ))}
 
@@ -283,12 +236,12 @@ export default function RequestClient() {
         </div>
 
         <Form.Group className="mb-3">
-          <AppTextInput
+          <AppTextArea
             label="詳細情報"
-            type="textarea"
             name="details"
             placeholder={`例）〇〇フェスで手に入る限定グッズをお願いしたいです。希望するアイテムは以下です。\n\nTシャツ（黒、Lサイズ）`}
             autoComplete="off"
+            rows={6}
             onChange={handleInputChange}
           />
         </Form.Group>
