419f653ca71ae900e0b076fea1e2edd566b52967
kikukawa <2201009@s.asojuku.ac.jp>
2024-10-18 12:01:45 +0900

    authGuardのエラーでてたやつ修正

--- frontend/src/app/authGuard.tsx
+++ frontend/src/app/authGuard.tsx
@@ -1,18 +1,26 @@
 'use client'
 
 import { useEffect, useCallback } from 'react'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { ReactNode } from 'react'
 import { useCurrentUserState } from '@/lib/jotai/userState'
 import { apiClient } from '@/lib/axios'
+import { getItem } from '@/utils/localStorage'
 
 export default function AuthGuard({ children }: { children: ReactNode }) {
   const { currentUser, setCurrentUser } = useCurrentUserState()
+  const pathname = usePathname()
+  console.log(pathname)
   const router = useRouter()
 
   const checkUser = useCallback(async () => {
+    const token = getItem('token')
     try {
-      const res = await apiClient.get('/user')
+      const res = await apiClient.get('/user', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
       const { user } = res.data
       if (user) {
         setCurrentUser(user)
@@ -21,15 +29,15 @@ export default function AuthGuard({ children }: { children: ReactNode }) {
       }
     } catch (error) {
       console.error('Failed to fetch user account:', error)
-      router.push('/')
+      router.push('/login')
     }
   }, [setCurrentUser, router])
 
   useEffect(() => {
-    if (!currentUser) {
+    if (pathname == '/request' || pathname == 'profile') {
       checkUser()
     }
-  }, [checkUser, currentUser])
+  }, [])
 
   return <>{children}</>
 }
