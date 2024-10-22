202e4043d98f1a124124df9e2607ea910c721125
masahiro <137256182+2201017@users.noreply.github.com>
2024-10-22 09:59:37 +0900

    出来ました

--- frontend/src/app/room/page.tsx
+++ frontend/src/app/room/page.tsx
@@ -1,9 +1,57 @@
-import { Container } from 'react-bootstrap'
+'use client'
 
-export default async function Room() {
+import Link from 'next/link'
+import { Button, Container, Form } from 'react-bootstrap'
+import { People, PersonCircle, BorderStyle } from 'react-bootstrap-icons'
+
+export default function Room() {
   return (
-    <Container>
-      <p>Room</p>
-    </Container>
+    <Form>
+      <Container>
+        <div className="d-flex justify-content-between">
+          <div className="d-flex">
+            <PersonCircle size={48} className="mb-4" />
+            <div>
+              <p className="mb-0">AAA</p>
+              <p className="mb-2">ありがとうございます。</p>
+            </div>
+          </div>
+          <div>
+            <p></p>
+            <p>14:25</p>
+          </div>
+        </div>
+      </Container>
+      <Container>
+        <div className="d-flex justify-content-between">
+          <div className="d-flex">
+            <PersonCircle size={48} className="mb-4" />
+            <div>
+              <p className="mb-0">BBB</p>
+              <p className="mb-2">ありがとうございます。</p>
+            </div>
+          </div>
+          <div>
+            <p></p>
+            <p>14:25</p>
+          </div>
+        </div>
+      </Container>
+      <Container>
+        <div className="d-flex justify-content-between">
+          <div className="d-flex">
+            <PersonCircle size={48} className="mb-4" />
+            <div>
+              <p className="mb-0">CCC</p>
+              <p className="mb-2">ありがとうございます。</p>
+            </div>
+          </div>
+          <div>
+            <p></p>
+            <p>14:25</p>
+          </div>
+        </div>
+      </Container>
+    </Form>
   )
 }
