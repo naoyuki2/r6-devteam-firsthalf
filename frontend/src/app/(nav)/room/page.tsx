4cecfd3b5947d551d18b33304d1f9e8a97fc91f8
naoyuki2 <kamaboko912@gmail.com>
2024-10-22 12:18:02 +0900

    修正

--- frontend/src/app/(nav)/room/page.tsx
+++ frontend/src/app/(nav)/room/page.tsx
@@ -1,57 +1,47 @@
-'use client'
-
-import Link from 'next/link'
-import { Button, Container, Form } from 'react-bootstrap'
-import { People, PersonCircle, BorderStyle } from 'react-bootstrap-icons'
+import { Container, Form } from 'react-bootstrap'
+import { PersonCircle } from 'react-bootstrap-icons'
 
 export default function Room() {
   return (
-    <Form>
-      <Container>
-        <div className="d-flex justify-content-between">
-          <div className="d-flex">
-            <PersonCircle size={48} className="mb-4" />
-            <div>
-              <p className="mb-0">AAA</p>
-              <p className="mb-2">ありがとうございます。</p>
-            </div>
-          </div>
+    <>
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex">
+          <PersonCircle size={48} className="mb-4" />
           <div>
-            <p></p>
-            <p>14:25</p>
+            <p className="mb-0">AAA</p>
+            <p className="mb-2 text-truncate">
+              ありがとうございます。ありがとうございます
+            </p>
           </div>
         </div>
-      </Container>
-      <Container>
-        <div className="d-flex justify-content-between">
-          <div className="d-flex">
-            <PersonCircle size={48} className="mb-4" />
-            <div>
-              <p className="mb-0">BBB</p>
-              <p className="mb-2">ありがとうございます。</p>
-            </div>
-          </div>
+        <div>
+          <p>14:25</p>
+        </div>
+      </div>
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex">
+          <PersonCircle size={48} className="mb-4" />
           <div>
-            <p></p>
-            <p>14:25</p>
+            <p className="mb-0">BBB</p>
+            <p className="mb-2">ありがとうございます。</p>
           </div>
         </div>
-      </Container>
-      <Container>
-        <div className="d-flex justify-content-between">
-          <div className="d-flex">
-            <PersonCircle size={48} className="mb-4" />
-            <div>
-              <p className="mb-0">CCC</p>
-              <p className="mb-2">ありがとうございます。</p>
-            </div>
-          </div>
+        <div>
+          <p>14:25</p>
+        </div>
+      </div>
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex">
+          <PersonCircle size={48} className="mb-4" />
           <div>
-            <p></p>
-            <p>14:25</p>
+            <p className="mb-0">CCC</p>
+            <p className="mb-2">ありがとうございます。</p>
           </div>
         </div>
-      </Container>
-    </Form>
+        <div>
+          <p>14:25</p>
+        </div>
+      </div>
+    </>
   )
 }
