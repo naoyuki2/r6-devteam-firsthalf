704a8b26f0018e55602a2f3b4f668c43c3d5b563
1901004 <166771485+1901004@users.noreply.github.com>
2024-10-28 11:52:32 +0900

    ごめ～ん

--- backend/src/module/room/room.controller.ts
+++ backend/src/module/room/room.controller.ts
@@ -14,20 +14,22 @@ import { roomSerializer } from './room.serializer'
 
 @Controller()
 export class RoomController {
-  private userService = new RoomService()
+  private roomService = new RoomService()
 
+  @Authorized()
   @Get(GetById.endpoint)
-  async getById(
-    @Req() req: Request<GetById.req, {}, {}, {}>,
-    @Res() res: Response<GetById.res>,
-  ) {
-    const { userId } = req.params
-    const roomUser = await this.userService.getByRoomUser({ userId })
-    const rooms = roomUser.room
-    const id = rooms.id
-    const room = await this.userService.getByRoom({ id })
+  async getById(@Req() req: Request, @Res() res: Response<GetById.res>) {
+    const userId = req.currentUserId!
+    const rooms = []
+    const roomUsers = await this.roomService.getByRoomUser({ userId })
+    for (let i: number = 0; i < roomUsers.length; i++) {
+      const room_user = roomUsers[i]
+      const { id } = room_user.room
+      const room = await this.roomService.getByRoom({ id })
+      rooms.push(room)
+    }
     return res.json({
-      room: roomSerializer(room),
+      rooms: rooms.map((room) => roomSerializer(room)),
     })
   }
 }
