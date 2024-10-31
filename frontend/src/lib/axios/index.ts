e6f72821d326c1350c32c6ca52b291db4ff36525
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2024-10-31 12:41:16 +0900

    修正

--- frontend/src/lib/axios/index.ts
+++ frontend/src/lib/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_HAKOBUN_API_URL || 'http://localhost:3030',
+  baseURL: process.env.NEXT_PUBLIC_HAKOBUN_API_URL,
   headers: {
     'Content-Type': 'application/json',
     // Authorization: 'Bearer your_token',
