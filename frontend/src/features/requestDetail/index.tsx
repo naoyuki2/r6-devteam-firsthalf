b3b58722c7b8b22c40f1c80ff1d31bbedd2646cb
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2024-10-31 12:39:36 +0900

    自分の依頼のチャットボタンを非表示にする

--- frontend/src/features/requestDetail/index.tsx
+++ frontend/src/features/requestDetail/index.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from 'next/navigation'
 import { apiClient } from '@/lib/axios'
 import { CreateRoomArgs, Request } from '@/types'
+import { useCurrentUser } from '@/lib/jotai/userState'
 
 type RequestDetailClientProps = {
   request: Request
@@ -12,6 +13,8 @@ export default function RequestDetailClient({
   request,
 }: RequestDetailClientProps) {
   const router = useRouter()
+  const currentUser = useCurrentUser()
+  const isChatShow = request.user.id == currentUser?.id
 
   const requestCreateRoom = async () => {
     const args: CreateRoomArgs = {
@@ -33,6 +36,8 @@ export default function RequestDetailClient({
     if (res == null) return
     router.push('/chat')
   }
+
+  if (isChatShow) return <></>
 
   return (
     <button className="btn btn-info" type="button" onClick={requestCreateRoom}>
