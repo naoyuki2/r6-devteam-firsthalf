fa40f1de6c3ebb261f9485529ca3aea7f709dfea
kikukawa <2201009@s.asojuku.ac.jp>
2024-11-05 14:57:42 +0900

    チャットに必要な情報の準備とプラスα

---
+++ frontend/src/utils/socket.ts
@@ -0,0 +1,37 @@
+import io, { Socket } from 'socket.io-client'
+
+export type MessageProps = {
+  roomId: string
+  body: string
+  userId: number
+}
+
+let socket: Socket | undefined
+
+const initializeSocket = () => {
+  if (!socket) {
+    socket = io('http://localhost:3030')
+  }
+}
+
+export const joinRoom = (roomId: string) => {
+  initializeSocket()
+  socket?.emit('joinRoom', { roomId })
+}
+
+export const sendMessage = ({ roomId, body, userId }: MessageProps) => {
+  initializeSocket()
+  socket?.emit('sendMessage', { roomId, message: body, userId })
+}
+
+export const receiveMessage = (callback: (message: string) => void) => {
+  initializeSocket()
+  socket?.on('message', callback)
+}
+
+export const disconnectSocket = () => {
+  if (socket) {
+    socket.disconnect()
+    socket = undefined
+  }
+}
