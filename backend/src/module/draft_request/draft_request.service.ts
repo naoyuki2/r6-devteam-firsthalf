9445fd4e39dfe3d35568ac823d122880d08e8d57
naoyuki2 <kamaboko912@gmail.com>
2024-11-18 09:51:23 +0900

    リファクタ

--- backend/src/module/draft_request/draft_request.service.ts
+++ backend/src/module/draft_request/draft_request.service.ts
@@ -8,32 +8,32 @@ const draftRequestRepository = AppDataSource.getRepository(DraftRequest)
 
 export class DraftRequestService {
   async create(room: Room): Promise<DraftRequest> {
-    const draftRequest = RequestToDraftRequest(room)
+    const draftRequest = this._requestToDraftRequest(room)
     return await draftRequestRepository.save(draftRequest)
   }
-}
 
-function RequestToDraftRequest(room: Room): DraftRequest {
-  const draftRequest = new DraftRequest()
+  _requestToDraftRequest(room: Room): DraftRequest {
+    const draftRequest = new DraftRequest()
 
-  draftRequest.title = room.request.title
-  draftRequest.location_prefecture = room.request.location_prefecture
-  draftRequest.location_details = room.request.location_details
-  draftRequest.delivery_prefecture = room.request.delivery_prefecture
-  draftRequest.delivery_details = room.request.delivery_details
-  draftRequest.description = room.request.description
-  draftRequest.status = room.request.status
-  draftRequest.room = room
+    draftRequest.title = room.request.title
+    draftRequest.location_prefecture = room.request.location_prefecture
+    draftRequest.location_details = room.request.location_details
+    draftRequest.delivery_prefecture = room.request.delivery_prefecture
+    draftRequest.delivery_details = room.request.delivery_details
+    draftRequest.description = room.request.description
+    draftRequest.status = room.request.status
+    draftRequest.room = room
 
-  draftRequest.draft_items = room.request.items.map(ItemToDraftItem)
+    draftRequest.draft_items = room.request.items.map(this._itemToDraftItem)
 
-  return draftRequest
-}
+    return draftRequest
+  }
 
-function ItemToDraftItem(item: Item): DraftItem {
-  const draftItem = new DraftItem()
-  draftItem.name = item.name
-  draftItem.quantity = item.quantity
-  draftItem.price = item.price
-  return draftItem
+  _itemToDraftItem(item: Item): DraftItem {
+    const draftItem = new DraftItem()
+    draftItem.name = item.name
+    draftItem.quantity = item.quantity
+    draftItem.price = item.price
+    return draftItem
+  }
 }
