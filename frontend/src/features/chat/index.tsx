d4487271e1301dbcd9105f747cd8e75272c67e94
momo <momomo.momoko.0665@icloud.com>
2024-11-24 23:07:55 +0900

    やることリスト　作成
    数字アイコンのところがうまくいかなかったので放置

--- frontend/src/features/chat/index.tsx
+++ frontend/src/features/chat/index.tsx
@@ -4,7 +4,15 @@ import { useEffect, useState } from 'react'
 import { useCurrentUser } from '@/lib/jotai/userState'
 import { disconnectSocket, receiveMessage, joinRoom } from '@/utils/socket'
 import TopNav from '@/component/TopNav'
-import { Container, Spinner } from 'react-bootstrap'
+import {
+  Container,
+  Spinner,
+  Row,
+  Col,
+  Button,
+  ListGroup,
+} from 'react-bootstrap'
+import { PersonFill } from 'react-bootstrap-icons'
 import { MessageList, Room } from '@/types'
 import { handleSendMessage } from './utils'
 import { ChatMessage } from '@/component/ChatMessage'
@@ -12,6 +20,102 @@ import { useDraftRequest } from './hooks'
 import { AppAlert } from '@/component/AppAlert'
 import { AppTextArea } from '@/component/AppTextArea'
 import { AppButton } from '@/component/AppButton'
+
+const TaskStatus = () => {
+  return (
+    <Container fluid className="pt-4" style={{ backgroundColor: '#f8f9fa' }}>
+      <Row className="mb-4">
+        <Col>
+          <h5 className="mb-3 border-start border-info border-5 ps-3 fw-bold">
+            やること
+          </h5>
+          <ListGroup className="list-unstyled">
+            <ListGroup.Item className="border-0 p-0">
+              <span className="">1.</span>
+              右上のアイコンから依頼を確認して、不都合な点がないかチャットでやり取りしましょう。（依頼者は必要に応じて依頼を更新しましょう。）
+            </ListGroup.Item>
+            <ListGroup.Item className="border-0 p-0">
+              <span className="">2.</span>
+              不都合な点が解消されたら、合意ボタンを押しましょう。（※依頼者と運び手がともに合意ボタンを押すと依頼が締結されます。）
+            </ListGroup.Item>
+            <ListGroup.Item className="border-0 p-0">
+              <span className="">3.</span>
+              依頼が確定されたら、受け渡し日時・場所などをチャットでやり取りしましょう。
+            </ListGroup.Item>
+            <ListGroup.Item className="border-0 p-0">
+              <span className="">4.</span>
+              商品の受け渡しが終了したら、下から取引相手の評価をしましょう。
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <h5 className="mb-3 border-start border-info border-5 ps-3 fw-bold">
+            ユーザーのステータス
+          </h5>
+          <ListGroup>
+            <ListGroup.Item
+              className="d-flex align-items-center justify-content-between mb-3"
+              style={{
+                border: '1px solid #000',
+                borderRadius: '10px',
+                padding: '16px',
+              }}
+            >
+              <div className="d-flex align-items-center">
+                <PersonFill
+                  size={40}
+                  className="me-3 text-secondary"
+                  style={{ backgroundColor: '#e9ecef', borderRadius: '50%' }}
+                />
+                <div>
+                  <div className="fw-bold">テストAさん</div>
+                  <div className="text-muted">依頼者</div>
+                </div>
+              </div>
+              <div className="d-flex align-items-center">
+                <span className="me-3">合意していません</span>
+                <Button variant="outline-success">合意</Button>
+              </div>
+            </ListGroup.Item>
+
+            <ListGroup.Item
+              className="d-flex align-items-center justify-content-between mb-3"
+              style={{
+                border: '1px solid #000',
+                borderRadius: '10px',
+                padding: '16px',
+              }}
+            >
+              <div className="d-flex align-items-center">
+                <PersonFill
+                  size={40}
+                  className="me-3 text-secondary"
+                  style={{ backgroundColor: '#e9ecef', borderRadius: '50%' }}
+                />
+                <div>
+                  <div className="fw-bold">テストBさん</div>
+                  <div className="text-muted">運び手</div>
+                </div>
+              </div>
+              <div className="d-flex align-items-center">
+                <span className="me-3">合意していません</span>
+                <Button
+                  variant="outline-success"
+                  style={{ visibility: 'hidden', borderRadius: '20px' }}
+                >
+                  合意
+                </Button>
+              </div>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
 
 const ChatClient = ({ room }: { room: Room }) => {
   const currentUser = useCurrentUser()
@@ -58,6 +162,7 @@ const ChatClient = ({ room }: { room: Room }) => {
   return (
     <>
       <TopNav draftRequest={draftRequest} otherRole={room.otherUser.role} />
+      <TaskStatus />
       <Container className="vh-100 d-flex flex-column">
         <div style={{}}>
           <ChatMessage
